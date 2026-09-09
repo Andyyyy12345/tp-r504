0f1696e85eee01a2fc28e7a2754dec65fa1ceae4
xiongand <xiongand@univ-rouen.fr>
2026-09-09 17:24:19 +0200

    fonction prenant 2 arguments entiers a et b et qui va renvoyer a^b

---
+++ TP1/fonctions.py
@@ -0,0 +1,10 @@
+def puissance(a, b):
+    return a ** b
+
+a = int(input("Entrez le nombre a : "))
+b = int(input("Entrez la puissance b : "))
+
+resultat = puissance(a, b)
+
+print("Le résultat est :", resultat)
+
